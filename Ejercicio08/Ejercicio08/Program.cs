8f04958eda3df1b204abeae356807d9a9682261d
diego.taub <diego.taub@accenture.com>
2021-03-20 18:35:11 -0300

    Ejercicio08

---
+++ Ejercicio08/Ejercicio08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una frase: ");
+            if (Console.ReadLine().Contains("fin"))
+            {
+                Console.Write("La frase contiene la palabra 'fin'");
+            }
+            else
+            {
+                Console.Write("La frase no contiene la palabra 'fin'");
+            }
+            Console.ReadKey();
+        }
+    }
+}
